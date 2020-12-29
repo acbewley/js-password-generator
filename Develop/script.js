01d8cbf0db21de645d7050ad9a5707210bd801eb
acbewley <acbewley13@gmail.com>
2020-12-29 17:41:30 -0500

    added prompts for password length and characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var charNum = prompt("How many characters? (Must be between 8 and 128)")
+
+  if (charNum < 8) {
+    alert("Passwords must have no less than 8 characters.")
+    return;
+  } else if (charNum > 128) {
+    alert("Passwords must have no more than 128 characters.")
+    return;
+  }
+
+  var lowerCase = confirm("Would you like lowercase letters?")
+  var upperCase = confirm("Would you like uppercase letters?")
+  var numbers = confirm("Would you like numbers?")
+  var specChar = confirm("Would you like special characters?")
+  
+  console.log(lowerCase)
+  console.log(upperCase)
+  console.log(numbers)
+  console.log(specChar)
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
